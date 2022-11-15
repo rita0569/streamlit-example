7d9ea40b004c73ed47253f9d926039b6c3236cb5
rita0569 <103230223+rita0569@users.noreply.github.com>
2022-11-15 22:33:33 +0900

    文字コードを追加

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,5 @@ uploaded_file = st.file_uploader("ファイルアップロード", type='csv')
 st.header('読み込みデータ表示')
 if uploaded_file is not None:
     # アップロードファイルをメイン画面にデータ表示
-    df = pd.read_csv(uploaded_file)
+    df = pd.read_csv(uploaded_file,encoding=shift-jis)
     st.write(df)
