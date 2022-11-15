6fb84b099b3195961bbea6fd31dcf41073c43681
rita0569 <103230223+rita0569@users.noreply.github.com>
2022-11-15 22:03:58 +0900

    タイトル修正

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title(”Zaim集計アプリ")
+st.title(u8”zaim集計アプリ")
 
 uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=False)
 for uploaded_file in uploaded_files:
