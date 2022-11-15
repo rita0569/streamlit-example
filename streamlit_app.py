6393c61761e673e63f8d3dcf03dd12c6bcd9c9ac
rita0569 <103230223+rita0569@users.noreply.github.com>
2022-11-15 22:02:53 +0900

    csv入力部分を追加

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
-st.title("テストだよ")
+st.title(”Zaim集計アプリ")
+
+uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=False)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
